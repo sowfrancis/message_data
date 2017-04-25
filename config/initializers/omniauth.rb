4831838b7ff3a00b06d3ba2565ff1ba450bace5a
sowfrancis <sow.francis@gmail.com>
2017-04-25 16:32:20 +0200

    redirect uri google modif

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
-OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://message-data-1.herokuapp.com/' : 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
